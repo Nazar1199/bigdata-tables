f7e55c3ba5360a63f56ef04abfd52be37b4aa35f
Nazar Ignatenko <naignatenko.ext@avito.ru>
2026-01-17 22:10:11 +0300

    upd

--- frontend/src/api/items.api.ts
+++ frontend/src/api/items.api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL || `${window.location.origin}/api`;
+const baseURL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL,
